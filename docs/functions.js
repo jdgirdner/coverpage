967390b029b5ce33a31154b5d5926d3df1899f0a
isaac-j-miller <44651730+isaac-j-miller@users.noreply.github.com>
2019-11-04 19:21:40 -0500

    added gallery, our mission
    our mission written by Justyn Girdner

--- docs/functions.js
+++ docs/functions.js
@@ -1,3 +1,33 @@
+
+const slides = 
+[
+    {
+        src:"images/gallery/team.jpg",
+        caption:"The team poses for a picture with the first iteration of the drone"
+    },
+    {
+        src:"images/gallery/team_winner.jpg",
+        caption:"The team wins 1st place at the Madison Engineering xChange, Spring 2019"
+    },
+    {
+        src:"images/gallery/dog.jpg",
+        caption:"Momo, Jacob's dog"
+    }
+]
+var slidesIndex = -1;
+
 function disappearHeader(){
     $("header").fadeOut(1000);
+}
+function plusSlides(increment){
+    slidesIndex +=increment;
+    if(slidesIndex < 0){
+        slidesIndex = slides.length - 1;
+    }
+    else if (slidesIndex >= slides.length){
+        slidesIndex = 0;
+    }
+    $("#gallery-slideshow").css("background-image", "url("+slides[slidesIndex].src+")");
+    $("#image-caption").text(slides[slidesIndex].caption + " ("+(slidesIndex+1).toString()+"/"+slides.length.toString()+")");
+
 }
